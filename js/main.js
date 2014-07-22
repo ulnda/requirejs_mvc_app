85423332c9eb48c5cd5c4c1e0cb943fd0845b4a0
ulnda <denis.nagibin@gmail.com>
2014-07-22 15:20:54 +0400

    Add simple MVC structure

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-require(['Models/User'], function(User){
+require(['Models/User', 'Controllers/ListController'], function(User, ListController) {
      
     var users = [new User('Barney'),
                  new User('Cartman'),
                  new User('Sheldon')];
      
-    for (var i = 0, len = users.length; i < len; i++){
-        console.log(users[i].name);
-    }
+    localStorage.users = JSON.stringify(users);
+
+    ListController.start();
 });
